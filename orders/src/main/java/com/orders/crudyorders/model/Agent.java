555c832027d3497e04fd5bfc1c3b712a3735f551
Jtonna <34466548+Jtonna@users.noreply.github.com>
2019-06-12 13:45:07 -0700

    Agent Setters and Getters

--- orders/src/main/java/com/orders/crudyorders/model/Agent.java
+++ orders/src/main/java/com/orders/crudyorders/model/Agent.java
@@ -28,4 +28,75 @@ public class Agent
                 orphanRemoval = true)
     private ArrayList<Order> orders = new ArrayList<>();
 
+    public Agent(String agentname, String workingarea, double comission, String phone, String country) {
+        this.agentname = agentname;
+        this.workingarea = workingarea;
+        this.comission = comission;
+        this.phone = phone;
+        this.country = country;
+    }
+
+    public long getAgentcode() {
+        return agentcode;
+    }
+
+//    public void setAgentcode(long agentcode) {
+//        this.agentcode = agentcode;
+//    }
+
+    public String getAgentname() {
+        return agentname;
+    }
+
+    public void setAgentname(String agentname) {
+        this.agentname = agentname;
+    }
+
+    public String getWorkingarea() {
+        return workingarea;
+    }
+
+    public void setWorkingarea(String workingarea) {
+        this.workingarea = workingarea;
+    }
+
+    public double getComission() {
+        return comission;
+    }
+
+    public void setComission(double comission) {
+        this.comission = comission;
+    }
+
+    public String getPhone() {
+        return phone;
+    }
+
+    public void setPhone(String phone) {
+        this.phone = phone;
+    }
+
+    public String getCountry() {
+        return country;
+    }
+
+    public void setCountry(String country) {
+        this.country = country;
+    }
+
+    public ArrayList<Customer> getCustomers() {
+        return customers;
+    }
+
+    public void setCustomers(ArrayList<Customer> customers) {
+        this.customers = customers;
+    }
+
+    public ArrayList<Order> getOrders() {
+        return orders;
+    }
+
+    public void setOrders(ArrayList<Order> orders) {
+        this.orders = orders;
+    }
 }
