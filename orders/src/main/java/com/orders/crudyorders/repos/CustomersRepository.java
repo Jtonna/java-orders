5dc3295d6d3f78369e32ea89e7d11b8baa1a8f5c
Jtonna <34466548+Jtonna@users.noreply.github.com>
2019-06-12 12:40:35 -0700

    Update CustomersRepository.java

--- orders/src/main/java/com/orders/crudyorders/repos/CustomersRepository.java
+++
@@ -1,8 +0,0 @@
-package com.orders.crudyorders.repos;
-
-import com.orders.crudyorders.model.Customers;
-import org.springframework.data.repository.CrudRepository;
-
-public interface CustomersRepository extends CrudRepository<Customers, Long> {
-    Customers findCustomersByCustName(String custName);
-}
